3215f7663724899a4400ea5e2f907492b2f6e96c
hota1024 <hotalog@hota1024.com>
2020-07-06 07:28:08 +0900

    🏷️ Add ElseToken type

---
+++ src/types/tokens/reserveds/ElseToken.ts
@@ -0,0 +1,6 @@
+import { TokenBase } from '../../TokenBase'
+
+/*
+ * ElseToken type.
+ */
+export type ElseToken = TokenBase<'else', 'reserved'>
--- src/types/tokens/reserveds/index.ts
+++ src/types/tokens/reserveds/index.ts
@@ -1,3 +1,4 @@
 export * from './ConstToken'
+export * from './ElseToken'
 export * from './IfToken'
 export * from './LetToken'
