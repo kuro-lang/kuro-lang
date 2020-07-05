cad4d10dad04a6ee3215d47dc471f3133347412a
hota1024 <hotalog@hota1024.com>
2020-07-06 08:37:55 +0900

    🏷️ Add document comment

--- src/utils/useEquals.ts
+++ src/utils/useEquals.ts
@@ -1,5 +1,7 @@
-/*
+/**
  * Returns a function that checks if the value is the same as the argument passed.
+ *
+ * @param value Value.
  */
 export const useEquals = <T>(value: T) => (passed: T): boolean =>
   value === passed
