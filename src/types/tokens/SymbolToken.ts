22726ab52976b699207d27e4eb2dad7c46018a12
hota1024 <hotalog@hota1024.com>
2020-07-06 07:54:08 +0900

    🏷️ Add SymbolToken type

---
+++ src/types/tokens/SymbolToken.ts
@@ -0,0 +1,71 @@
+import {
+  AsteriskAsteriskEqualsToken,
+  AsteriskAsteriskToken,
+  AsteriskEqualsToken,
+  AsteriskToken,
+  ColonToken,
+  CommaToken,
+  DotDotToken,
+  DotToken,
+  EqualsEqualsToken,
+  EqualsToken,
+  ExclamationEqualsToken,
+  GreaterEqualsThanToken,
+  GreaterThanToken,
+  LeftBraceToken,
+  LeftBracketToken,
+  LeftParenToken,
+  LessEqualsThanToken,
+  LessThanToken,
+  MinusEqualsToken,
+  MinusMinusToken,
+  MinusToken,
+  PercentEqualsToken,
+  PercentToken,
+  PlusEqualsToken,
+  PlusPlusToken,
+  PlusToken,
+  QuestionToken,
+  RightBraceToken,
+  RightBracketToken,
+  RightParenToken,
+  SlashEqualsToken,
+  SlashToken,
+} from './symbols'
+
+/*
+ * SymbolToken type.
+ */
+export type SymbolToken =
+  | AsteriskAsteriskEqualsToken
+  | AsteriskAsteriskToken
+  | AsteriskEqualsToken
+  | AsteriskToken
+  | ColonToken
+  | CommaToken
+  | DotDotToken
+  | DotToken
+  | EqualsEqualsToken
+  | EqualsToken
+  | ExclamationEqualsToken
+  | GreaterEqualsThanToken
+  | GreaterThanToken
+  | LeftBraceToken
+  | LeftBracketToken
+  | LeftParenToken
+  | LessEqualsThanToken
+  | LessThanToken
+  | MinusEqualsToken
+  | MinusMinusToken
+  | MinusToken
+  | PercentEqualsToken
+  | PercentToken
+  | PlusEqualsToken
+  | PlusPlusToken
+  | PlusToken
+  | QuestionToken
+  | RightBraceToken
+  | RightBracketToken
+  | RightParenToken
+  | SlashEqualsToken
+  | SlashToken
