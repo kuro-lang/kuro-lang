3bcffce107256d73ca4bc0baabb0ee31eb4bccb2
hota1024 <hotalog@hota1024.com>
2020-07-12 21:38:32 +0900

    🏷️ Add StringLiteralRule class

---
+++ src/impls/lexerRules/literals/StringLiteralRule.ts
@@ -0,0 +1,42 @@
+import { CappedRule } from '../../../abstracts'
+import { StringLiteralToken } from '../../../types'
+import { ILoc } from '../../../interfaces'
+import { LocatedError } from '../../errors'
+
+/*
+ * UnterminatedStringLiteralError class.
+ */
+export class UnterminatedStringLiteralError extends LocatedError {
+  name = 'UnterminatedStringLiteralError'
+}
+
+/*
+ * StringLiteralRule class.
+ */
+export class StringLiteralRule extends CappedRule<StringLiteralToken> {
+  constructor() {
+    super([
+      {
+        start: '"',
+        end: '"',
+        allowNewLines: false,
+        allowEscapes: true,
+      },
+    ])
+  }
+
+  createToken(value: string, loc: ILoc): StringLiteralToken {
+    return {
+      kind: 'string_literal',
+      value,
+      loc,
+    }
+  }
+
+  createUnterminatedError(loc: ILoc): LocatedError {
+    return new UnterminatedStringLiteralError(
+      'unterminated string literal',
+      loc
+    )
+  }
+}
