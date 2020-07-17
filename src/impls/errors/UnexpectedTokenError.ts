b31dd4d37b287f432704a42f29acb958a7607452
hota1024 <hotalog@hota1024.com>
2020-07-17 17:28:28 +0900

    🏷️ Add UnexpectedTokenError class

---
+++ src/impls/errors/UnexpectedTokenError.ts
@@ -0,0 +1,8 @@
+import { LocatedError } from '.'
+
+/*
+ * UnexpectedTokenError class.
+ */
+export class UnexpectedTokenError extends LocatedError {
+  name = 'UnexpectedTokenError'
+}
--- src/impls/errors/index.ts
+++ src/impls/errors/index.ts
@@ -1,1 +1,2 @@
 export * from './LocatedError'
+export * from './UnexpectedTokenError'
