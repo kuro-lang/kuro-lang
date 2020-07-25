8804e9e5f6f05ab61a08469cdb0fae93c4163f4a
hota1024 <hotalog@hota1024.com>
2020-07-25 20:48:16 +0900

    🏷️ Add StringLiteralExpression type

---
+++ src/types/nodes/expressions/literals/StringLiteralExpression.ts
@@ -0,0 +1,9 @@
+import { LiteralNodeBase } from '../../..'
+
+/**
+ * StringLiteralExpression type.
+ */
+export type StringLiteralExpression = LiteralNodeBase<
+  'string_literal_expression',
+  string
+>
--- src/types/nodes/expressions/literals/index.ts
+++ src/types/nodes/expressions/literals/index.ts
@@ -1,1 +1,2 @@
 export * from './NumericLiteralExpression'
+export * from './StringLiteralExpression'
