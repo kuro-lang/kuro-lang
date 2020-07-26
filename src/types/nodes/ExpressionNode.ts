67df2c05da1a919e3ea6384946679c006f6e844d
hota1024 <hotalog@hota1024.com>
2020-07-26 20:46:41 +0900

    🏷️ Add CallExpression

--- src/types/nodes/ExpressionNode.ts
+++ src/types/nodes/ExpressionNode.ts
@@ -2,12 +2,14 @@ import {
   LiteralExpression,
   PropertyAccessExpression,
   ElementAccessExpression,
+  CallExpression,
 } from './expressions'
 
 /**
  * Expression type.
  */
 export type Expression =
+  | CallExpression
   | ElementAccessExpression
   | LiteralExpression
   | PropertyAccessExpression
