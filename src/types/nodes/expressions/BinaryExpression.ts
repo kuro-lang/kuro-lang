cbbc98a486c0d0de995a22ee49fc320c8ca3c4d9
hota1024 <hotalog@hota1024.com>
2020-07-26 20:48:57 +0900

    🏷️ Add BinaryExpression type

---
+++ src/types/nodes/expressions/BinaryExpression.ts
@@ -0,0 +1,23 @@
+import { NodeBase } from '../..'
+import { Token } from '../../Token'
+import { Expression } from '../ExpressionNode'
+
+/**
+ * BinaryExpression type.
+ */
+export type BinaryExpression = NodeBase<'binary_expression'> & {
+  /**
+   * Operator token.
+   */
+  operator: Token
+
+  /**
+   * Left expression node.
+   */
+  left: Expression
+
+  /**
+   * Right expression node.
+   */
+  right: Expression
+}
--- src/types/nodes/expressions/index.ts
+++ src/types/nodes/expressions/index.ts
@@ -1,5 +1,6 @@
 export * from './literals'
 
+export * from './BinaryExpression'
 export * from './CallExpression'
 export * from './ElementAccessExpression'
 export * from './LiteralExpression'
