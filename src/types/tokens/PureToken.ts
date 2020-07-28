d4935e9ad25bd4eadccc1e8ec3443400e908c455
hota1024 <hotalog@hota1024.com>
2020-07-28 17:20:58 +0900

    🏷️ Add NewLineToken to PureToken type

--- src/types/tokens/PureToken.ts
+++ src/types/tokens/PureToken.ts
@@ -1,6 +1,7 @@
 import { SymbolToken, ReservedToken } from '.'
+import { NewLineToken } from './NewLineToken'
 
 /*
  * PureToken type.
  */
-export type PureToken = ReservedToken | SymbolToken
+export type PureToken = ReservedToken | SymbolToken | NewLineToken
