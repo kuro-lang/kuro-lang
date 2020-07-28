a5d1fd531c8f27c319f5b412d91e72b20e547120
hota1024 <hotalog@hota1024.com>
2020-07-28 16:20:13 +0900

    🏷️ Add nodes

--- src/types/Node.ts
+++ src/types/Node.ts
@@ -1,6 +1,13 @@
 import { Expression, Statement } from '.'
+import { Identifier, Parameter, PropertyAssignment, Root } from './nodes'
 
 /**
  * AST Node type.
  */
-export type Node = Expression | Statement
+export type Node =
+  | Expression
+  | Statement
+  | Identifier
+  | Parameter
+  | PropertyAssignment
+  | Root
