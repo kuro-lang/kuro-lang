741f0edafae5fdac0956340fd53bb8312f80840e
hota1024 <hotalog@hota1024.com>
2020-08-05 21:33:51 +0900

    🏷️ Add parser exports and DI register

--- src/impls/parsers/index.ts
+++ src/impls/parsers/index.ts
@@ -1,1 +1,22 @@
+import { container } from 'tsyringe'
+import { ParserToken } from '../..'
+import {
+  LiteralExpressionParser,
+  ArrayLiteralExpressionParser,
+} from './expressions'
+
+export * from './expressions'
 export * from './IdentifierParser'
+
+const registerParsers = () => {
+  container.register(
+    ParserToken.LiteralExpressionParser,
+    LiteralExpressionParser
+  )
+  container.register(
+    ParserToken.ArrayLiteralParser,
+    ArrayLiteralExpressionParser
+  )
+}
+
+registerParsers()
