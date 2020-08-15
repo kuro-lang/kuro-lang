e38eee923178e8bcbd12ba096236680d3be23046
hota1024 <hotalog@hota1024.com>
2020-08-15 16:09:36 +0900

    🔥 Remove BinaryExpressionParser class

--- src/impls/parsers/expressions/BinaryExpressionParser.ts
+++
@@ -1,8 +0,0 @@
-import { IParser, Expression, TokenWalker, SourceCode } from '../../..'
-
-/**
- * BinaryExpressionParser class.
- */
-export class BinaryExpressionParser implements IParser<Expression> {
-  parse(source: SourceCode, walker: TokenWalker): Expression {}
-}
