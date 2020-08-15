106912d6beced1e42d1e82ac22ea42f2e2577ccb
hota1024 <hotalog@hota1024.com>
2020-08-15 19:09:33 +0900

    🏷️ Add BinaryExpressionParser class

---
+++ src/abstracts/parsers/BinaryExpressionParser.ts
@@ -0,0 +1,49 @@
+import { IParser, Expression } from '../..'
+import { SourceCode, TokenWalker, TokenKind } from '../../types'
+import { loop } from '../../utils'
+
+/**
+ * BinaryExpressionParser class.
+ */
+export abstract class BinaryExpressionParser implements IParser<Expression> {
+  /**
+   * Sub parser.
+   */
+  abstract subParser: IParser<Expression>
+
+  /**
+   * Token kinds.
+   */
+  abstract kinds: TokenKind[]
+
+  validate(): boolean {
+    return true
+  }
+
+  parse(source: SourceCode, walker: TokenWalker): Expression {
+    let expression = this.subParser.parse(source, walker)
+
+    return loop(({ end }) => {
+      const peek = walker.peek()
+
+      if (!peek) {
+        return end(expression)
+      }
+
+      if (this.kinds.includes(peek.kind)) {
+        walker.next()
+        const right = this.subParser.parse(source, walker)
+
+        expression = {
+          kind: 'binary_expression',
+          operator: peek,
+          left: expression,
+          right,
+          loc: expression.loc.merge(right.loc),
+        }
+      } else {
+        return end(expression)
+      }
+    })
+  }
+}
