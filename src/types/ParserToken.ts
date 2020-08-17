a0085dd1e943f49c7a1dbafd837c79db4981f8a4
hota1024 <hotalog@hota1024.com>
2020-08-17 17:59:02 +0900

    🔥 Clear ParserToken

--- src/types/ParserToken.ts
+++ src/types/ParserToken.ts
@@ -1,29 +1,4 @@
 /**
  * ParserToken.
  */
-export const ParserToken = {
-  /**
-   * ExpressionParser token.
-   */
-  ExpressionParser: 'ExpressionParser',
-
-  /**
-   * LiteralExpressionParser token.
-   */
-  LiteralExpressionParser: 'LiteralExpressionParser',
-
-  /**
-   * ArrayLiteralParser token.
-   */
-  ArrayLiteralParser: 'ArrayLiteralParser',
-
-  /**
-   * BinaryExpressionParser token.
-   */
-  BinaryExpressionParser: 'BinaryExpressionParser',
-
-  /**
-   * StatementParser token.
-   */
-  StatementParser: 'StatementParser',
-} as const
+export const ParserToken = {} as const
