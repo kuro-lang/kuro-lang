4bc7a33b73ab39775207176347847870a40f38d1
hota1024 <hotalog@hota1024.com>
2020-08-19 19:48:04 +0900

    🏷️ Add Some parser tokens

--- src/types/ParserToken.ts
+++ src/types/ParserToken.ts
@@ -4,6 +4,54 @@
 export const ParserToken = {
   /**
    * AtomParser token.
+   *
+   * Parse
+   * - `<literal_expression>`
+   * - `(<expression>)`
    */
   Atom: Symbol('Atom'),
+
+  /**
+   * AssignExpressionParser token.
+   *
+   * Parse
+   * - `<expression> = <expression>`
+   */
+  AssignExpression: Symbol('AssignExpression'),
+
+  /**
+   * OrExpressionParser token.
+   *
+   * Parse
+   * - `<expression> || <expression>`
+   */
+  OrExpression: Symbol('LogicalOrExpression'),
+
+  /**
+   * AndExpressinParser token.
+   *
+   * Parse
+   * - `<expression> && <expression>`
+   */
+  AndExpression: Symbol('LogicalAndExpression'),
+
+  /**
+   * EquivalentExpressionParser token.
+   *
+   * Parse
+   * - `<expression> == <expression>`
+   * - `<expression> != <expression>`
+   */
+  EquivalentExpression: Symbol('LogicalEquivalentExpression'),
+
+  /**
+   * ComparisonExpressionParser token.
+   *
+   * Parse
+   * - `<expression> < <expression>`
+   * - `<expression> <= <expression>`
+   * - `<expression> > <expression>`
+   * - `<expression> >= <expression>`
+   */
+  ComparisonExpression: Symbol('ComparisonExpression'),
 } as const
