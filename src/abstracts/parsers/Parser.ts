f21eb7eeb453a1a63c3420a778a79a917593eb62
hota1024 <hotalog@hota1024.com>
2020-08-19 23:19:55 +0900

    🏷️ Add createPeekError method

--- src/abstracts/parsers/Parser.ts
+++ src/abstracts/parsers/Parser.ts
@@ -1,9 +1,23 @@
 import { IParser } from '../..'
 import { SourceCode, TokenWalker, Node } from '../../types'
+import { LocatedError } from '../../impls'
 
 /**
  * Parser abstract class.
  */
 export abstract class Parser implements IParser {
   abstract parse(source: SourceCode, walker: TokenWalker): Node
+
+  /**
+   * Returns a LocatedError object that has a message of peek error.
+   *
+   * @param source SourceCode object.
+   * @param walker Token walker.
+   */
+  protected createPeekError(
+    source: SourceCode,
+    walker: TokenWalker
+  ): LocatedError {
+    return new LocatedError(`peek error`, walker.locTo(-1))
+  }
 }
