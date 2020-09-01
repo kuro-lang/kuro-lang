b5f9cf54fd2d859c6741b37928bc5bbb356c3b01
hota1024 <hotalog@hota1024.com>
2020-09-01 17:33:48 +0900

    🏷️ Add parserContainer.ts

---
+++ src/impls/parsers/parserContainer.ts
@@ -0,0 +1,17 @@
+import { Container } from 'inversify'
+import getDecorators from 'inversify-inject-decorators'
+
+/**
+ * Parser DI container.
+ */
+export const parserContainer = new Container()
+
+/**
+ * DI decorators.
+ */
+export const decorators = getDecorators(parserContainer)
+
+/**
+ * Parser injection decorator.
+ */
+export const injectParser = decorators.lazyInject
