a52acdc1284ebf92ce93a74f376f9ae5b73d17d1
hota1024 <hotalog@hota1024.com>
2020-09-02 22:55:20 +0900

    🏷️ Add EquivalentParser class

---
+++ src/impls/parsers/expressions/EquivalentParser.ts
@@ -0,0 +1,15 @@
+import { BinaryExpressionParser } from '../../..'
+import { injectable } from 'inversify'
+import { injectParser } from '../parserContainer'
+import { ParserToken, Expression, PureTokenKind } from '../../../types'
+import { IParser } from '../../../interfaces'
+
+/**
+ * EquivalentParser class.
+ */
+@injectable()
+export class EquivalentParser extends BinaryExpressionParser {
+  @injectParser(ParserToken.Comparsion) subParser: IParser<Expression>
+
+  kinds: PureTokenKind[] = ['equals_equals', 'exclamation_equals']
+}
