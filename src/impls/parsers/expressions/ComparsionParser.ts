4ae2d7165464ef659ecd4330d66a888a21560963
hota1024 <hotalog@hota1024.com>
2020-09-02 22:54:24 +0900

    🏷️ Add ComparsionParser class

---
+++ src/impls/parsers/expressions/ComparsionParser.ts
@@ -0,0 +1,20 @@
+import { BinaryExpressionParser } from '../../..'
+import { injectable } from 'inversify'
+import { injectParser } from '../parserContainer'
+import { ParserToken, Expression, PureTokenKind } from '../../../types'
+import { IParser } from '../../../interfaces'
+
+/**
+ * Comparsion class.
+ */
+@injectable()
+export class Comparsion extends BinaryExpressionParser {
+  @injectParser(ParserToken.AddAndSub) subParser: IParser<Expression>
+
+  kinds: PureTokenKind[] = [
+    'less_than',
+    'less_equals_than',
+    'greater_than',
+    'greater_equals_than',
+  ]
+}
