ea45faf88f3058f53ffe37051fc3f5265af860c8
hota1024 <hotalog@hota1024.com>
2020-09-02 17:59:50 +0900

    🏷️ Add GroupParser class

---
+++ src/impls/parsers/expressions/GroupParser.ts
@@ -0,0 +1,49 @@
+import { Parser, Expression } from '../../..'
+import { injectParser } from '../parserContainer'
+import { ParserToken } from '../../../types'
+import { IParser } from '../../../interfaces'
+import { TokenWalker } from '../../../classes'
+import { injectable } from 'inversify'
+
+/**
+ * GroupParser class.
+ */
+@injectable()
+export class GroupParser extends Parser<Expression> {
+  /**
+   * ExpressionsParser.
+   */
+  @injectParser(ParserToken.Expressions) expressions: IParser<Expression>
+
+  /**
+   * AtomParser.
+   */
+  @injectParser(ParserToken.Atom) atom: IParser<Expression>
+
+  parse(walker: TokenWalker): Expression {
+    const peek = walker.peek()
+
+    if (!peek) {
+      throw this.createPeekError(walker)
+    }
+
+    if (peek.kind === 'left_paren') {
+      walker.next()
+
+      const node = this.expressions.parse(walker)
+      const token = walker.next()
+
+      if (!token) {
+        throw this.createPeekError(walker)
+      }
+
+      if (token.kind === 'right_paren') {
+        return node
+      }
+
+      throw this.createUnexpectedError(token, walker, ')')
+    }
+
+    return this.atom.parse(walker)
+  }
+}
