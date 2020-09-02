a8d8b6cf560bb2c5f0816a08ab8f03f37fa7120f
hota1024 <hotalog@hota1024.com>
2020-09-02 22:55:52 +0900

    🏷️ Add OrParser class

---
+++ src/impls/parsers/expressions/OrParser.ts
@@ -0,0 +1,15 @@
+import { BinaryExpressionParser } from '../../..'
+import { injectable } from 'inversify'
+import { injectParser } from '../parserContainer'
+import { ParserToken, Expression, PureTokenKind } from '../../../types'
+import { IParser } from '../../../interfaces'
+
+/**
+ * OrParser class.
+ */
+@injectable()
+export class OrParser extends BinaryExpressionParser {
+  @injectParser(ParserToken.Equivalent) subParser: IParser<Expression>
+
+  kinds: PureTokenKind[] = ['bar_bar']
+}
