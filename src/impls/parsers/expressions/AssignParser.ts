47916c451583c452e21708e870ba1cbf3ed567c5
hota1024 <hotalog@hota1024.com>
2020-09-03 11:27:01 +0900

    🏷️ Add AssignParser class

---
+++ src/impls/parsers/expressions/AssignParser.ts
@@ -0,0 +1,23 @@
+import { BinaryExpressionParser } from '../../..'
+import { injectable } from 'inversify'
+import { injectParser } from '../parserContainer'
+import { ParserToken, Expression, PureTokenKind } from '../../../types'
+import { IParser } from '../../../interfaces'
+
+/**
+ * AssignParser class.
+ */
+@injectable()
+export class AssignParser extends BinaryExpressionParser {
+  @injectParser(ParserToken.And) subParser: IParser<Expression>
+
+  kinds: PureTokenKind[] = [
+    'equals',
+    'plus_equals',
+    'minus_equals',
+    'asterisk_equals',
+    'asterisk_asterisk_equals',
+    'slash_equals',
+    'percent_equals',
+  ]
+}
