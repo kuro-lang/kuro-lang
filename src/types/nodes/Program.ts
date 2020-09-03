e8ec6435a5858c8760d6fc8cd4adb92a5a52b748
hota1024 <hotalog@hota1024.com>
2020-09-03 21:08:37 +0900

    🏷️ Add source field

--- src/types/nodes/Program.ts
+++ src/types/nodes/Program.ts
@@ -1,9 +1,15 @@
 import { NodeBase, Statement } from '..'
+import { SourceCode } from '../SourceCode'
 
 /**
  * Program type.
  */
 export type Program = NodeBase<'program'> & {
+  /**
+   * SourceCode object.
+   */
+  source: SourceCode
+
   /**
    * Statements.
    */
