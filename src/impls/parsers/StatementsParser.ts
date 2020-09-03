e0edbb59226d4bbd2fd2effbdc911ebc13f9ec10
hota1024 <hotalog@hota1024.com>
2020-09-03 20:58:41 +0900

    🏷️ Add Statementsparser

---
+++ src/impls/parsers/StatementsParser.ts
@@ -0,0 +1,192 @@
+import { Parser, Expression } from '../..'
+import { injectable } from 'inversify'
+import { injectParser } from './parserContainer'
+import { ParserToken, Statement } from '../../types'
+import { IParser } from '../../interfaces'
+import { TokenWalker } from '../../classes'
+import { Identifier } from '../../../types'
+import { loop } from '../../utils'
+
+/**
+ * StatementsParser class.
+ */
+@injectable()
+export class StatementsParser extends Parser<Statement> {
+  /**
+   * ExpressionsParser.
+   */
+  @injectParser(ParserToken.Expressions) expressions: IParser<Expression>
+
+  parse(walker: TokenWalker): Statement {
+    const peek = walker.peek()
+    this.forwardToPrimaryToken(walker)
+
+    if (!peek) {
+      throw this.createPeekError(walker)
+    }
+
+    if (peek.kind === 'break') {
+      const next = walker.next()
+
+      if (!next || next.kind === 'semi_colon' || next.kind === 'new_line') {
+        return {
+          kind: 'break_statement',
+          loc: peek.loc,
+        }
+      }
+
+      const expression = this.expressions.parse(walker)
+      this.forwardToPrimaryToken(walker)
+
+      return {
+        kind: 'break_statement',
+        loc: peek.loc.merge(expression.loc),
+      }
+    }
+
+    if (peek.kind === 'return') {
+      const next = walker.next()
+
+      if (!next || next.kind === 'semi_colon' || next.kind === 'new_line') {
+        return {
+          kind: 'return_statement',
+          loc: peek.loc,
+        }
+      }
+
+      const expression = this.expressions.parse(walker)
+      this.forwardToPrimaryToken(walker)
+
+      return {
+        kind: 'return_statement',
+        loc: peek.loc.merge(expression.loc),
+      }
+    }
+
+    if (peek.kind === 'continue') {
+      walker.next()
+      this.forwardToPrimaryToken(walker)
+
+      return {
+        kind: 'continue_statement',
+        loc: peek.loc,
+      }
+    }
+
+    if (peek.kind === 'let') {
+      const identifierToken = walker.next()
+
+      if (!identifierToken) {
+        throw this.createPeekError(walker)
+      }
+
+      if (identifierToken.kind !== 'identifier') {
+        throw this.createUnexpectedError(identifierToken, walker, 'identifier')
+      }
+
+      const identifier: Identifier = {
+        ...identifierToken,
+      }
+      const equals = walker.next()
+
+      if (!equals || equals.kind !== 'equals') {
+        return {
+          kind: 'let_statement',
+          identifier,
+          loc: peek.loc.merge(identifier.loc),
+        }
+      }
+
+      const initializer = this.expressions.parse(walker)
+      this.forwardToPrimaryToken(walker)
+
+      return {
+        kind: 'let_statement',
+        identifier,
+        initializer,
+        loc: peek.loc.merge(initializer.loc),
+      }
+    }
+
+    if (peek.kind === 'const') {
+      const identifierToken = walker.next()
+
+      if (!identifierToken) {
+        throw this.createPeekError(walker)
+      }
+
+      if (identifierToken.kind !== 'identifier') {
+        throw this.createUnexpectedError(identifierToken, walker, 'identifier')
+      }
+
+      const identifier: Identifier = {
+        ...identifierToken,
+      }
+      const equals = walker.next()
+
+      if (!equals) {
+        throw this.createPeekError(walker)
+      }
+
+      if (equals.kind === 'equals') {
+        throw this.createUnexpectedError(equals, walker, '=')
+      }
+
+      const expression = this.expressions.parse(walker)
+      this.forwardToPrimaryToken(walker)
+
+      return {
+        kind: 'const_statement',
+        identifier,
+        initializer: expression,
+        loc: peek.loc.merge(expression.loc),
+      }
+    }
+
+    if (peek.kind === 'left_brace') {
+      walker.next()
+      const statements: Statement[] = []
+
+      loop(({ end }) => {
+        const peek = this.forwardToPrimaryToken(walker)
+
+        if (!peek) {
+          throw this.createPeekError(walker)
+        }
+
+        if (peek.kind === 'right_brace') {
+          return end()
+        }
+
+        const statement = this.parse(walker)
+        statements.push(statement)
+      })
+    }
+
+    const expression = this.expressions.parse(walker)
+
+    return {
+      kind: 'expression_statement',
+      expression,
+      loc: expression.loc,
+    }
+  }
+
+  /**
+   * Forward walker to primary token.
+   *
+   * @param walker Token walker.
+   */
+  protected forwardToPrimaryToken(
+    walker: TokenWalker
+  ): ReturnType<TokenWalker['peek']> {
+    let peek = walker.peek()
+
+    while (peek && (peek.kind === 'semi_colon' || peek.kind === 'new_line')) {
+      walker.next()
+      peek = walker.peek()
+    }
+
+    return peek
+  }
+}
