eb77e93f30a363ba686eefe53aad1157cb8d1be0
hota1024 <hotalog@hota1024.com>
2020-09-07 14:11:00 +0900

    🏷️ Add BlockParser class

---
+++ src/impls/parsers/BlockParser.ts
@@ -0,0 +1,65 @@
+import { Parser, loop } from '../..'
+import { injectParser } from './parserContainer'
+import { ParserToken, BlockStatement, Statement } from '../../types'
+import { IParser } from '../../interfaces'
+import { TokenWalker } from '../../classes'
+import { injectable } from 'inversify'
+
+/**
+ * BlockParser class.
+ */
+@injectable()
+export class BlockParser extends Parser<BlockStatement> {
+  /**
+   * StatementParser.
+   */
+  @injectParser(ParserToken.Statements) statements: IParser<Statement>
+
+  parse(walker: TokenWalker): BlockStatement {
+    const leftBraceToken = walker.next()
+    console.log('block')
+    console.log(leftBraceToken)
+
+    if (!leftBraceToken) {
+      throw this.createPeekError(walker)
+    }
+
+    if (leftBraceToken.kind !== 'left_brace') {
+      throw this.createUnexpectedError(leftBraceToken, walker, '{')
+    }
+
+    const statements: Statement[] = []
+
+    loop(({ end }) => {
+      const peek = this.forwardToPrimaryToken(walker)
+
+      if (!peek) {
+        throw this.createPeekError(walker)
+      }
+
+      if (peek.kind === 'right_brace') {
+        return end()
+      }
+
+      console.log(peek)
+      const statement = this.statements.parse(walker)
+      statements.push(statement)
+    })
+
+    const rightBraceToken = walker.next()
+
+    if (!rightBraceToken) {
+      throw this.createPeekError(walker)
+    }
+
+    if (rightBraceToken.kind !== 'right_brace') {
+      throw this.createUnexpectedError(rightBraceToken, walker, '}')
+    }
+
+    return {
+      kind: 'block_statement',
+      body: statements,
+      loc: leftBraceToken.loc.merge(rightBraceToken.loc),
+    }
+  }
+}
